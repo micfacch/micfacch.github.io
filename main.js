a24220f14d03cdcb07c329a44e68c766b94759d4
micfacch <micfacch@gmail.com>
2013-12-31 16:53:08 -0700

    with icons

---
+++ main.js
@@ -0,0 +1,24 @@
+$(function(){
+    $(".next-chapter").bind("click", function (event) {
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
+        var target = $(this).attr("href");
+        $("html, body").stop().animate({
+                scrollLeft: $(target).offset().left,
+                scrollTop: $(target).offset().top
+            }, 500);
+    });
+});
+
+    $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 310){  
+       // x should be from where you want this to happen from top//
+        //make CSS changes here  
+         $('header').addClass('scrolled');
+         $('#logo').attr('src','MFloGoBlack.png');
+      } 
+      else{
+        //back to default styles
+        $('header').removeClass('scrolled');
+        $('#logo').attr('src','MFloGoWhite.png');
+      }
+    });
