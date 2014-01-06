d87772e51437d2645b5a0fcaf1a9fed306b14403
micfacch <micfacch@gmail.com>
2014-01-06 15:29:19 -0600

    js fix 1million

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".next-chapter").bind("click", function (event) {
-        event.preventDefault ? event.preventDefault() : event.preventDefault = false;
+        event.preventDefault ? event.preventDefault() : event.returnValue = false;
         var target = $(this).attr("href");
         $("html, body").stop().animate({
                 scrollLeft: $(target).offset().left,
@@ -14,11 +14,11 @@ $(function(){
        // x should be from where you want this to happen from top//
         //make CSS changes here  
          $('header').addClass('scrolled');
-         $('#logo').attr('src','MFLoGoBlack.png');
+         $('#logo').attr('src','./MFLoGOBlack.png');
       } 
       else{
         //back to default styles
         $('header').removeClass('scrolled');
-        $('#logo').attr('src','MFLoGoWhite.png');
+        $('#logo').attr('src','./MFLoGOWhite.png');
       }
     });
