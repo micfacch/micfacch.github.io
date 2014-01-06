5b04b2a5d8162c0ae43efeb2b844420f05fddeae
micfacch <micfacch@gmail.com>
2014-01-06 15:17:08 -0600

    js 3

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ $(function(){
        // x should be from where you want this to happen from top//
         //make CSS changes here  
          $('header').addClass('scrolled');
-         $('#logo').attr('src','./MFLoGoBlack.png');
+         $('#logo').attr('src','MFLoGoBlack.png');
       } 
       else{
         //back to default styles
         $('header').removeClass('scrolled');
-        $('#logo').attr('src','./MFLoGoWhite.png');
+        $('#logo').attr('src','MFLoGoWhite.png');
       }
     });
